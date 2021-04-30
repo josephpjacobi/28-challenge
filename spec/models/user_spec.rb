e8a41fa750ca87c0d794c1e6a59fcf63732646f7
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-04-30 16:02:16 -0400

    added user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User Creation" do
+    context "in a state where we offer services" do
+      it "should include all attributes" do
+      
+      end
+
+      it "should create a user when the age requirement is met" do
+        
+      end
+
+      it "should NOT create a user when the age requirement is NOT met" do
+        
+      end
+    end
+
+    context "in a stat where we do not offer services" do
+      it " should not create a user" do
+        
+      end
+    end
+  end
 end
