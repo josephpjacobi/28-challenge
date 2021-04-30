9697091def318250af3ccf7ba487c00d4fcc5db1
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-04-30 16:14:50 -0400

    added product model

---
+++ db/migrate/20210430201439_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :category
+      t.string :ndc
+      t.integer :qty
+      t.integer :price
+      t.string :instructions
+
+      t.timestamps
+    end
+  end
+end
