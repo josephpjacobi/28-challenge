ddc7bd61a72537d6d895d2ec0efde2c9f9a5918a
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-05-03 20:42:19 -0400

    added display route

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def display
+    @full_name = display_params[:full_name]
+    @email = display_params[:email]
+  end
+end
+
+private
+
+def display_params
+  params.permit(:full_name, :email)
 end
