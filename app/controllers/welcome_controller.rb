f3cddd2a57cba4b3d590ff13e585d5f6e095a4b3
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-05-04 09:32:45 -0400

    added date of birth to form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+    @states = State.all
   end
 
   def display
     @full_name = display_params[:full_name]
     @email = display_params[:email]
+    @date_of_birth = display_params[:date_of_birth]
   end
 end
 
 private
 
 def display_params
-  params.permit(:full_name, :email)
+  params.permit(:full_name, :email, :date_of_birth)
 end
